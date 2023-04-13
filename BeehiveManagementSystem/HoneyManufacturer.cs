471a759b2b80b7897edb07b89db75c59cf30761f
brandon.michaud <brandon.michaud@PS-00RLY221400C>
2023-04-13 12:23:22 -0400

    Add project files.

---
+++ BeehiveManagementSystem/HoneyManufacturer.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BeehiveManagementSystem
+{
+    class HoneyManufacturer:Bee
+    {
+        public const float NECTAR_PROCESSED_PER_SHIFT = 33.15f; 
+        public override float CostPerShift
+        { get { return 1.7f; } }
+
+        protected override void DoJob()
+        {
+            //This Bee subclass overrides DoJob to call the HoneyVault method to convert nectar to honey
+            HoneyVault.ConvertNectarToHoney(NECTAR_PROCESSED_PER_SHIFT);
+        }
+
+        public HoneyManufacturer() : base("Honey Manufacturer") { }
+    }
+}
---
+++ BeehiveManagementSystem/HoneyVault.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BeehiveManagementSystem
+{
+    static class HoneyVault
+    {
+        private const float NECTAR_CONVERSION_RATIO = .19f;
+        private const float LOW_LEVEL_WARNING = 10f;
+        private static float honey = 25f;
+        private static float nectar = 100f;
+        public static string StatusReport
+        {
+            get
+            {
+                string statusString = "";
+                statusString += $"{honey:0.0} units of honey\n{nectar:0.0} units of nectar\n";
+                if (honey < LOW_LEVEL_WARNING)
+                    statusString += "LOW HONEY - ADD A HONEY MANUFACTURER\n";
+                if (nectar < LOW_LEVEL_WARNING)
+                    statusString += "LOW NECTAR - ADD A NECTAR COLLECTOR\n";
+
+                return statusString;
+            }
+        }
+
+
+
+        public static void CollectNectar(float amount)
+        {
+            if (amount > 0f) nectar += amount;
+        }
+
+        public static void ConvertNectarToHoney(float amount)
+        {
+            float nectarToConvert = amount;
+
+            if (nectarToConvert > nectar) nectarToConvert = nectar;
+
+            nectar -= nectarToConvert;
+            honey += nectarToConvert * NECTAR_CONVERSION_RATIO;
+        }
+
+        public static bool ConsumeHoney(float amount)
+        {
+            if (amount <= honey)
+            { 
+                honey -= amount;
+                return true;
+            }
+            else { return false; }
+
+        }
+    }
+}
